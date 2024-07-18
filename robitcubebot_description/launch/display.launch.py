79d96de807bb84d5096d7e0b283d47bc2268b6a2
omarabelgwad <120633090+omarabelgwad@users.noreply.github.com>
2024-07-19 01:10:42 +0300

    STLS

--- robitcubebot_description/launch/display.launch.py
+++ robitcubebot_description/launch/display.launch.py
@@ -1,47 +1,41 @@
 import os
+
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
+from launch.substitutions import LaunchConfiguration
 from launch.actions import DeclareLaunchArgument
-from launch.substitutions import Command, LaunchConfiguration
-
 from launch_ros.actions import Node
-from launch_ros.parameter_descriptions import ParameterValue
+
+import xacro
 
 
 def generate_launch_description():
-    robitcubebot_description_dir = get_package_share_directory("robitcubebot_description")
 
-    model_arg = DeclareLaunchArgument(name="model", default_value=os.path.join(
-                                        robitcubebot_description_dir, "urdf", "robitcubebot.urdf.xacro"
-                                        ),
-                                      description="Absolute path to robot urdf file")
+    # Check if we're told to use sim time
+    use_sim_time = LaunchConfiguration('use_sim_time')
 
-    robot_description = ParameterValue(Command(["xacro ", LaunchConfiguration("model")]),
-                                       value_type=str)
-
-    robot_state_publisher_node = Node(
-        package="robot_state_publisher",
-        executable="robot_state_publisher",
-        parameters=[{"robot_description": robot_description}]
+    # Process the URDF file
+    pkg_path = os.path.join(get_package_share_directory('robitcubebot_description'))
+    xacro_file = os.path.join(pkg_path,'urdf','robitcubebot.urdf.xacro')
+    robot_description_config = xacro.process_file(xacro_file)
+    
+    # Create a robot_state_publisher node
+    params = {'robot_description': robot_description_config.toxml(), 'use_sim_time': use_sim_time}
+    node_robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[params]
     )
 
-    joint_state_publisher_gui_node = Node(
-        package="joint_state_publisher_gui",
-        executable="joint_state_publisher_gui"
-    )
 
-    rviz_node = Node(
-        package="rviz2",
-        executable="rviz2",
-        name="rviz2",
-        output="screen",
-        arguments=["-d", os.path.join(robitcubebot_description_dir, "rviz", "display.rviz")],
-    )
-
+    # Launch!
     return LaunchDescription([
-        model_arg,
-        joint_state_publisher_gui_node,
-        robot_state_publisher_node,
-        rviz_node
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='false',
+            description='Use sim time if true'),
+
+        node_robot_state_publisher
     ])
